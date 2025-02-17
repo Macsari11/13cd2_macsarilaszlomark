04d977634fc3f92945453e74c919e330b2386b4a
Macsari11 <maclas871@hengersor.hu>
2025-02-17 14:31:52 +0100

    select hasznalata

--- server.js
+++ server.js
@@ -9,21 +9,43 @@ app.use(bodyparser.json());
 const db = mysql.createConnection(
     {
         user: "root",
-        host:"localhost",
-        port:"3307",
-        password:"",
-        database:"teliolimpia"
+        host: "localhost",
+        port: "3307",
+        password: "",
+        database: "teliolimpia"
 
     }
 )
 //a szerver futasanak ellenorzese
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
 
     res.send("a szerver mukodik!")
 
 
 })
+app.get("/v", (req, res) => {
+
+    const sql = "SELECT * FROM `versenyzok` WHERE 1"
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+}
+)
+
+
+app.get("/v6", (req, res) => {
+
+    const sql = "SELECT * FROM `versenyzok` WHERE ID=6;"
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+}
+)
 
 
 app.listen(3000, () => {

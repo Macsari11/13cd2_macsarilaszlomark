7551cf05907a0b436801a2681e2465df1f53dfa3
Macsari11 <maclas871@hengersor.hu>
2025-02-17 14:14:29 +0100

    szerver beallitasa
    mysql szerver kapcsolatanak letrehozasa.
    Ellenorzo get megirasa

--- server.js
+++ server.js
@@ -6,7 +6,24 @@ const bodyparser = require("body-parser");
 app.use(cors());
 app.use(bodyparser.json());
 
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host:"localhost",
+        port:"3307",
+        password:"",
+        database:"teliolimpia"
 
+    }
+)
+//a szerver futasanak ellenorzese
+
+app.get("/", (req,res) => {
+
+    res.send("a szerver mukodik!")
+
+
+})
 
 
 app.listen(3000, () => {
